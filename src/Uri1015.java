5bd330156d190321f5699e16ba427ce86383449e
werike <werike.ms@gmail.com>
2020-02-12 19:05:04 -0300

    Distância Entre Dois Pontos

--- src/Uri1015.java
+++ src/Uri1015.java
@@ -5,7 +5,7 @@ public class Uri1015 {
 
 	public static void main(String[] args) {
 		
-		/* URI Online Judge | 1015 - Distância Entre Dois Pontos - FAZENDO
+		/* URI Online Judge | 1015 - Distância Entre Dois Pontos - OK
 		 * 
 		 * Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1) e 
 		 * p2(x2,y2) e calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a fórmula:
