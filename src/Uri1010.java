478d8b8785ab83351a835d92cc90fcdf45cd056c
werike <werike.ms@gmail.com>
2020-02-12 19:47:44 -0300

    Cálculo Simples

--- src/Uri1010.java
+++ src/Uri1010.java
@@ -11,6 +11,7 @@ public class Uri1010 {
 		 * valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e
 		 * o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
 		 * 
+		 * 
 		 * Entrada:
 		 * 
 		 * O arquivo de entrada contém duas linhas de dados. Em cada linha haverá 3
