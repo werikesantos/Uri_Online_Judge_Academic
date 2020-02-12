82316ce2e801e64be360341617d0039b8dcdfe88
werike <werike.ms@gmail.com>
2020-02-12 19:35:50 -0300

    Área do Círculo

--- src/Uri1002.java
+++ src/Uri1002.java
@@ -30,6 +30,7 @@ public class Uri1002 {
 		 * 2.00                                         A=12.5664
 		 * 100.64                                       A=31819.3103
 		 * 150.00                                       A=70685.7750
+		 * 
 		 */
 		
 		Locale.setDefault(Locale.US); 
