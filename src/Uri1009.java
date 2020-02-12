9f3b13fcf35a2c8394d01920640d1f8c3ddf1df8
werike <werike.ms@gmail.com>
2020-02-12 19:46:00 -0300

    Salário com Bônus

--- src/Uri1009.java
+++ src/Uri1009.java
@@ -10,6 +10,7 @@ public class Uri1009 {
 		 * Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por 	     * ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre suas vendas 
 		 * efetuadas, informar o total a receber no final do mês, com duas casas decimais.
 		 * 
+		 * 
 		 * Entrada:
 		 * 
 		 * O arquivo de entrada contém um texto (primeiro nome do vendedor) e 2 valores de dupla precisão 
